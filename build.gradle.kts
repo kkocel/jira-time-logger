c4e9533b75d91e352398e3e2cf42b4ad8246e190
Krzysztof Kocel <krzysztof.kocel@gmail.com>
2022-12-20 10:43:47 +0100

    Jvm target 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,13 @@ kotlin {
     }
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
 repositories {
     mavenCentral()
 }
